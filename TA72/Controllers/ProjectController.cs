264e7ffef4b8bf0ba7d5b37bb6a0017c5cc88d99
thoory <tomtom.ory@gmail.com>
2020-01-07 21:23:37 +0100

    + Serialization of project

---
+++ TA72/Controllers/ProjectController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TA72.Models;
+
+namespace TA72.Controllers
+{
+    class ProjectController
+    {
+        public ProjectController(){}
+
+        public Project create(string name, string desc) {
+            Project p;
+
+            p = new Project(name, desc);
+
+            return p;
+        }
+
+        public String getName(Project p)
+        {
+            return p.name;
+        }
+        public String getDesc(Project p)
+        {
+            return p.desc;
+        }
+
+        public void setName(Project p, String name)
+        {
+            p.name = name;
+            p.lastupdate = DateTime.Now;
+        }
+        public void setDesc(Project p, String desc)
+        {
+            p.desc = desc;
+            p.lastupdate = DateTime.Now;
+        }
+        public void save(Project p)
+        {
+            p.save();
+        }
+
+        public Project load(String path)
+        {
+            Serializer serializer = new Serializer();
+            Project p = serializer.load(path);
+
+            return p;
+        }
+    }
+}
